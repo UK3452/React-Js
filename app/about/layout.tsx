b4897767addac5b8469e98e16404dd1d2bb60cb3
Utkarsh Kumar <kumar6.utkarsh@gmail.com>
2023-08-28 12:55:42 +0530

    React-js files

---
+++ app/about/layout.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+import React from "react";
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <nav>About NavBar</nav>
+      <main className={styles.main}>{children}</main>
+    </>
+  )
+}
