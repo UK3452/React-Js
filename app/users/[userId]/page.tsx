5136073f1206cc075352856dcfd4c91ac5cd424a
Utkarsh Kumar <kumar6.utkarsh@gmail.com>
2023-09-09 11:23:10 +0530

    Added Not Found Page and fixes

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -3,6 +3,9 @@ import getUserPosts from "@/lib/getUserPosts";
 import { Suspense } from "react";
 import UserPosts from "./components/UserPosts";
 import type { Metadata } from "next";
+import getAllUsers from "@/lib/getAllUsers";
+
+import { notFound } from "next/navigation";
 
 type Params = {
   params: {
@@ -15,6 +18,12 @@ export async function generateMetadata({
 }: Params): Promise<Metadata> {
   const userData: Promise<User> = getUser(userId);
   const user: User = await userData;
+
+  if (!user?.name) {
+    return {
+      title: "User Not Found",
+    };
+  }
 
   return {
     title: user.name,
@@ -29,6 +38,8 @@ export default async function UserPage({ params: { userId } }: Params) {
 
   const user = await userData;
 
+  if (!user?.name) notFound();
+
   return (
     <>
       <h2>{user.name}</h2>
@@ -38,4 +49,12 @@ export default async function UserPage({ params: { userId } }: Params) {
       </Suspense>
     </>
   );
+}
+
+export async function generateStaticParams() {
+  const usersData: Promise<User[]> = getAllUsers();
+  const users = await usersData;
+  return users.map((user) => ({
+    userId: user.id.toString(),
+  }));
 }
